82d98f628a784a3112c67b7312c62e0a214f7694
Siddhant Patwa <siddhantpatwa8@gmail.com>
2024-03-20 02:33:01 +0530

    msg

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CitySelector from './components/States/States';
+
+function App() {
+  return (
+    <div className="App">
+      <CitySelector />
+    </div>
+  );
+}
+
+export default App;
